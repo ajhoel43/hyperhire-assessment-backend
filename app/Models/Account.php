58c87ae9bfa445e2e8c4caff0f20564d1b3e3993
Ajhoel Tragedy <panduwiranata43@gmail.com>
2025-11-24 05:43:43 +0700

    feat(models): Account Model  - add scope to select liked & disliked accounts

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Account extends Model
 {
@@ -30,5 +31,29 @@ class Account extends Model
     {
         return $query->leftJoin('like_counters AS lc', 'accounts.id', '=', 'lc.account_id')
             ->select('accounts.*', 'lc.like_count', 'lc.dislike_count');
+    }
+
+    public function scopeLikedAccounts($query)
+    {
+        $likedQuery = DB::table('like_logs')
+            ->select('account_id')
+            ->where('action', 'like')
+            ->distinct();
+
+        return $query->joinSub($likedQuery, 'll', function($join) {
+            $join->on('accounts.id', '=', 'll.account_id');
+        })->select('accounts.*');
+    }
+
+    public function scopeDislikedAccounts($query)
+    {
+        $likedQuery = DB::table('like_logs')
+            ->select('account_id')
+            ->where('action', 'dislike')
+            ->distinct();
+
+        return $query->joinSub($likedQuery, 'll', function($join) {
+            $join->on('accounts.id', '=', 'll.account_id');
+        })->select('accounts.*');
     }
 }
