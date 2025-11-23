2ccf427a0c6f3f8b39039d7858d73a8732dbb515
Ajhoel Tragedy <panduwiranata43@gmail.com>
2025-11-24 06:48:24 +0700

    feat(controller): Account  - send email to dummy target  - get account first before increase like counter

--- app/Http/Controllers/Api/AccountController.php
+++ app/Http/Controllers/Api/AccountController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\AccountCheckpointReachedMail;
 use App\Models\Account;
 use App\Models\LikeCounter;
 use App\Models\LikeLog;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class AccountController extends Controller
 {
@@ -25,6 +27,11 @@ class AccountController extends Controller
     public function likeAccount(Request $request, $accountId): JsonResponse
     {
         try {
+            $account = Account::find($accountId);
+            if (!$account) {
+                throw new \Exception("Account not found.");
+            }
+
             $hasLike = LikeCounter::where('account_id', $accountId)->first();
             if ($hasLike) {
                 // Update Existing
@@ -44,7 +51,12 @@ class AccountController extends Controller
                 'action'     => 'like'
             ]);
 
-            // TODO: When an account has like > 50, send notification to admin
+            // When an account has reach 50 likes, send notification to admin
+            if ($hasLike && $hasLike->like_count + 1 === 50) {
+                // Send Email to Admin
+                Mail::to(env("MAIL_DUMMY_TARGET", "dummy@ajulity.com"))->send(new AccountCheckpointReachedMail($account));
+            }
+
             return response()->json([
                 'message' => "Account with ID {$accountId} has been liked."
             ]);
@@ -59,6 +71,11 @@ class AccountController extends Controller
     public function dislikeAccount(Request $request, $accountId): JsonResponse
     {
         try {
+            $account = Account::find($accountId);
+            if (!$account) {
+                throw new \Exception("Account not found.");
+            }
+
             $hasLike = LikeCounter::where('account_id', $accountId)->first();
             if ($hasLike) {
                 // Update Existing
