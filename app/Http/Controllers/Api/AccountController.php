5814b4eea9efe213c1910f114208bae39d2238fe
Ajhoel Tragedy <panduwiranata43@gmail.com>
2025-11-24 05:15:59 +0700

    feat(controller): Account Controller  - add basic data operation

--- app/Http/Controllers/Api/AccountController.php
+++ app/Http/Controllers/Api/AccountController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Account;
+use App\Models\LikeCounter;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -10,24 +12,68 @@ class AccountController extends Controller
 {
     public function getRecommendations(Request $request): JsonResponse
     {
+        $dataPerPage = $request->input('limit', 10);
+        $data = Account::withLikeCounts()->simplePaginate($dataPerPage);
+
         return response()->json([
-            'message' => 'This is a placeholder response for recommendations.'
+            'message' => 'This is a placeholder response for recommendations.',
+            'peoples' => $data,
         ]);
     }
 
     public function likeAccount(Request $request, $accountId): JsonResponse
     {
-        // TODO: When an account has like > 50, send notification to admin
-        return response()->json([
-            'message' => "Account with ID {$accountId} has been liked."
-        ]);
+        try {
+            $hasLike = LikeCounter::where('account_id', $accountId)->first();
+            if ($hasLike) {
+                // Update Existing
+                $hasLike->increment('like_count');
+            } else {
+                // Create New
+                LikeCounter::create([
+                    'account_id'    => $accountId,
+                    'like_count'    => 1,
+                    'dislike_count' => 0
+                ]);
+            }
+
+            // TODO: When an account has like > 50, send notification to admin
+            return response()->json([
+                'message' => "Account with ID {$accountId} has been liked."
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Cannot Like this account right now.',
+                'error'   => $th->getMessage()
+            ]);
+        }
     }
 
     public function dislikeAccount(Request $request, $accountId): JsonResponse
     {
-        return response()->json([
-            'message' => "Account with ID {$accountId} has been disliked."
-        ]);
+        try {
+            $hasLike = LikeCounter::where('account_id', $accountId)->first();
+            if ($hasLike) {
+                // Update Existing
+                $hasLike->increment('dislike_count');
+            } else {
+                // Create New
+                LikeCounter::create([
+                    'account_id'    => $accountId,
+                    'like_count'    => 0,
+                    'dislike_count' => 1
+                ]);
+            }
+
+            return response()->json([
+                'message' => "Account with ID {$accountId} has been disliked."
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Cannot Dislike this account right now.',
+                'error'   => $th->getMessage()
+            ]);
+        }
     }
 
     public function getLikedAccounts(Request $request): JsonResponse
