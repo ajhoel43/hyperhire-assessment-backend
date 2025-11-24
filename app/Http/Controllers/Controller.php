c3d45cbef2861f1834d1cbc78cf6afc4d5134f1d
Ajhoel Tragedy <panduwiranata43@gmail.com>
2025-11-24 07:15:25 +0700

    feat(swagger): Add swagger basic info

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ * version="1.0.0",
+ * title="Hyperhire Assessment API Documentation",
+ * description="Base API for Hypehire Assessment",
+ * @OA\Contact(
+ * email="support@ajulity.com"
+ * )
+ * )
+ * 
+ * @OA\Server(
+ * url=L5_SWAGGER_CONST_HOST,
+ * description="Hyperhire Assessment API Server"
+ * )
+ * 
+ */
 abstract class Controller
 {
     //
